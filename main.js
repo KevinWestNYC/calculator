a49ae5e1d0eeffab04c5bd44472cacfb1a884045
Kevin W <kevinwestnyc@gmail.com>
2021-06-29 11:56:05 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// function to calculate result
+//function to clear
+//function to put result to result area
+
+const one = document.getElementById("1-button");
+const two = document.getElementById("2-button");
+const three = document.getElementById("3-button");
+const four = document.getElementById("4-button");
+const five = document.getElementById("5-button");
+const six = document.getElementById("6-button");
+const seven = document.getElementById("7-button");
+const eight = document.getElementById("8-button");
+const nine = document.getElementById("9-button");
+const zero = document.getElementById("0-button");
+const decimal = document.getElementById("decimal-button");
+const divide = document.getElementById("divide-button");
+const multiply = document.getElementById("multiply-button");
+const minus = document.getElementById("minus-button");
+const add = document.getElementById("add-button");
+const equals = document.getElementById("equals-button");
+const clear = document.getElementById("clear-button");
+
