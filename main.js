262fce68048df257c4037ba5b23135c9aa9e229e
Kevin W <kevinwestnyc@gmail.com>
2021-06-30 13:23:50 -0400

    rearranged comments

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 //function to get numbers and operators
 // function to calculate result
 //function to clear
+//function to confirm only one "." registers per number
+//function to confirm there is only one operator at a time
 //function to put result to result area
 
-// const one = document.getElementById("1-button");
-// const two = document.getElementById("2-button");
-// const three = document.getElementById("3-button");
-// const four = document.getElementById("4-button");
-// const five = document.getElementById("5-button");
-// const six = document.getElementById("6-button");
-// const seven = document.getElementById("7-button");
-// const eight = document.getElementById("8-button");
-// const nine = document.getElementById("9-button");
-// const zero = document.getElementById("0-button");
-// const decimal = document.getElementById("decimal-button");
-// const divide = document.getElementById("divide-button");
-// const multiply = document.getElementById("multiply-button");
-// const minus = document.getElementById("minus-button");
-// const add = document.getElementById("add-button");
-// const equals = document.getElementById("equals-button");
-// const clear = document.getElementById("clear-button");
+/////////Logic/////////
+    // 1 get the first number to equal numberA and display
+    // 2 get the arithmetic operator and make it equal operatorA
+    // 3 get the second number equal numberB and display
+    // 4 then, based on the arithmetic operator pressed after numberB:
+    //     a* if operatorB is =, display the solution and make it equal numberA, then back to step2
+    //     b* if operatorB is + or - , display the total(which becomes the next numberA) and then the third number input becomes numberB, is displayed, then back to step 4 
+    //     c* if operatorB is / or x , await numberC to do the operation on numberB and numberC:
+    //         5a* if +, -, or = is pressed, operate on the 3 numbers to equal the new numberA, then back to step 2
+    //         5b* if / or x is pressed, multiply or divide numberB and number C to create new numberB, then back to step 4
+
+    // make sure only one "." registers per number
+    // make sure there is only one operator at a time
+    // Press C to delete all inputs at any time
+
+
 
 let numberA = 0;
 let numberB = 0;
@@ -47,21 +48,30 @@ function getNumberAndOperatorFromCalculator (){
 }
 
 
-    // 1 get the first number to equal numberA and display
-    // 2 get the arithmetic operator and make it equal operatorA
-    // 3 get the second number equal numberB and display
-    // 4 then, based on the arithmetic operator pressed after numberB:
-    //     a* if operatorB is =, display the solution and make it equal numberA, then back to step2
-    //     b* if operatorB is + or - , display the total(which becomes the next numberA) and then the third number input becomes numberB, is displayed, then back to step 4 
-    //     c* if operatorB is / or x , await numberC to do the operation on numberB and numberC:
-    //         5a* if +, -, or = is pressed, operate on the 3 numbers to equal the new numberA, then back to step 2
-    //         5b* if / or x is pressed, multiply or divide numberB and number C to create new numberB, then back to step 4
 
-    // make sure only one "." registers per number
-    // make sure there is only one operator at a time
-    // Press C to delete all inputs at any time
 
-        // if(numberA === 0){
+
+    // const one = document.getElementById("1-button");
+// const two = document.getElementById("2-button");
+// const three = document.getElementById("3-button");
+// const four = document.getElementById("4-button");
+// const five = document.getElementById("5-button");
+// const six = document.getElementById("6-button");
+// const seven = document.getElementById("7-button");
+// const eight = document.getElementById("8-button");
+// const nine = document.getElementById("9-button");
+// const zero = document.getElementById("0-button");
+// const decimal = document.getElementById("decimal-button");
+// const divide = document.getElementById("divide-button");
+// const multiply = document.getElementById("multiply-button");
+// const minus = document.getElementById("minus-button");
+// const add = document.getElementById("add-button");
+// const equals = document.getElementById("equals-button");
+// const clear = document.getElementById("clear-button");
+
+    
+
+    // if(numberA === 0){
     //     numberA = number;
     // } else if(numberA !==0 && numberB===0){
     //     numberB = number;
